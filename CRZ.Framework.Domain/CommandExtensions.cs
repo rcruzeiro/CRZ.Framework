158c7eb46fd6eb336dd30a4eddab40d5c991d324
Rafael Cruzeiro <rafael.cruzeiro@gmail.com>
2019-07-14 16:18:40 -0300

    include Command as a Domain mechanism + CRZ.Framework.Domain v1.1

---
+++ CRZ.Framework.Domain/CommandExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRZ.Framework.Domain
+{
+    public static class CommandExtensions
+    {
+        public static void Validate(this ICommand command, ValidationContext context = null)
+        {
+            if (command == null) throw new ArgumentNullException(nameof(command));
+
+            command.Validate(context);
+        }
+    }
+}
---
+++ CRZ.Framework.Domain/ICommand.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRZ.Framework.Domain
+{
+    public interface ICommand : IValidatableObject
+    { }
+}

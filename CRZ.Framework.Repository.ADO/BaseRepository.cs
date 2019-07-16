35ee7230e1e0d2f3acc7b32f0448869d867a5683
Rafael Cruzeiro <rafael.cruzeiro@gmail.com>
2019-07-15 22:35:53 -0300

    include Framework.Repository.ADO with Dapper support

---
+++ CRZ.Framework.Repository.ADO/BaseRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace CRZ.Framework.Repository.ADO
+{
+    public abstract class BaseRepository<T> : IDisposable
+        where T : class
+    {
+        protected IDbConnection DbConnection { get; }
+
+        protected BaseRepository(IDbConnection dbConnection)
+        {
+            DbConnection = dbConnection ?? throw new ArgumentNullException(nameof(dbConnection));
+        }
+
+        public IEnumerable<T> Query(string sql, object param = null)
+        {
+            var result = DbConnection.Query<T>(sql, param);
+
+            return result;
+        }
+
+        public IEnumerable<T> Query<TMap>(string sql, Func<T, TMap, T> map, object param = null)
+            where TMap : class
+        {
+            var result = DbConnection.Query(sql, map, param);
+
+            return result;
+        }
+
+        public T QueryFirst(string sql, object param = null)
+        {
+            var result = DbConnection.QueryFirst<T>(sql, param);
+
+            return result;
+        }
+
+        public T QuerySingle(string sql, object param = null)
+        {
+            var result = DbConnection.QuerySingle<T>(sql, param);
+
+            return result;
+        }
+
+        public int Execute(string sql, IEnumerable<T> param = null)
+        {
+            var result = DbConnection.Execute(sql, param);
+
+            return result;
+        }
+
+        public int Execute(string sql, T param = null)
+        {
+            return Execute(sql, new[] { param });
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (DbConnection != null)
+                    DbConnection.Dispose();
+            }
+        }
+    }
+}
---
+++ CRZ.Framework.Repository.ADO/BaseRepositoryAsync.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CRZ.Framework.Repository.ADO
+{
+    public abstract class BaseRepositoryAsync<T> : BaseRepository<T>
+        where T : class
+    {
+        protected BaseRepositoryAsync(IDbConnection dbConnection)
+            : base(dbConnection)
+        { }
+
+        public async Task<IEnumerable<T>> QueryAsync(string sql, object param = null, CancellationToken cancellationToken = default)
+        {
+            var result = await DbConnection.QueryAsync<T>(sql, param);
+
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<TMap>(string sql, Func<T, TMap, T> map, object param = null, CancellationToken cancellationToken = default)
+            where TMap : class
+        {
+            var result = await DbConnection.QueryAsync(sql, map, param);
+
+            return result;
+        }
+
+        public async Task<T> QueryFirstAsync(string sql, object param = null, CancellationToken cancellationToken = default)
+        {
+            var result = await DbConnection.QueryFirstAsync<T>(sql, param);
+
+            return result;
+        }
+
+        public async Task<T> QuerySingleAsync(string sql, object param = null, CancellationToken cancellationToken = default)
+        {
+            var result = await DbConnection.QuerySingleAsync<T>(sql, param);
+
+            return result;
+        }
+
+        public async Task<int> ExecuteAsync(string sql, IEnumerable<T> param = null, CancellationToken cancellationToken = default)
+        {
+            var result = await DbConnection.ExecuteAsync(sql, param);
+
+            return result;
+        }
+
+        public async Task<int> ExecuteAsync(string sql, T param = null)
+        {
+            return await ExecuteAsync(sql, new[] { param });
+        }
+    }
+}

5abdcb8bf95278c1ac4070c5b73c7d5e79e4c7f7
Rafael Cruzeiro <rafael.cruzeiro@gmail.com>
2019-07-16 21:10:31 -0300

    include NoSQL repository

---
+++ CRZ.Framework.Repository.NoSQL.MongoDB/BaseRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using MongoDB.Driver;
+
+namespace CRZ.Framework.Repository.NoSQL.MongoDB
+{
+    public abstract class BaseRepository<T>
+        where T : class
+    {
+        readonly string _collection;
+
+        readonly IMongoDatabase _database;
+
+        protected IMongoCollection<T> Collection => _database.GetCollection<T>(_collection);
+
+        protected BaseRepository(MongoDbOptions options)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            _collection = options.Collection;
+            IMongoClient client = new MongoClient(options.ConnectionString);
+            _database = client.GetDatabase(options.Database);
+        }
+
+        public long CountDocuments(Expression<Func<T, bool>> filter)
+        {
+            return Collection.CountDocuments(filter);
+        }
+
+        public long CountDocuments(FilterDefinition<T> filter)
+        {
+            return Collection.CountDocuments(filter);
+        }
+
+        public IEnumerable<T> GetDocuments()
+        {
+            return Collection.Find(_ => true).ToEnumerable();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> filter)
+        {
+            return Collection.Find(filter).ToEnumerable();
+        }
+
+        public IEnumerable<T> Find(FilterDefinition<T> filter)
+        {
+            return Collection.Find(filter).ToEnumerable();
+        }
+
+        public T FindOne(Expression<Func<T, bool>> filter)
+        {
+            return Collection.Find(filter).FirstOrDefault();
+        }
+
+        public T FindOne(FilterDefinition<T> filter)
+        {
+            return Collection.Find(filter).FirstOrDefault();
+        }
+
+        public void Insert(IEnumerable<T> documents)
+        {
+            Collection.InsertMany(documents);
+        }
+
+        public void Insert(T document)
+        {
+            Collection.InsertOne(document);
+        }
+
+        public void Replace(Expression<Func<T, bool>> filter, T replacement)
+        {
+            Collection.ReplaceOne(filter, replacement);
+        }
+
+        public void Replace(FilterDefinition<T> filter, T replacement)
+        {
+            Collection.ReplaceOne(filter, replacement);
+        }
+
+        public void DeleteMany(Expression<Func<T, bool>> filter)
+        {
+            Collection.DeleteMany(filter);
+        }
+
+        public void DeleteMany(FilterDefinition<T> filter)
+        {
+            Collection.DeleteMany(filter);
+        }
+
+        public void Delete(Expression<Func<T, bool>> filter)
+        {
+            Collection.DeleteOne(filter);
+        }
+
+        public void Delete(FilterDefinition<T> filter)
+        {
+            Collection.DeleteOne(filter);
+        }
+    }
+}
---
+++ CRZ.Framework.Repository.NoSQL.MongoDB/MongoDbOptions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace CRZ.Framework.Repository.NoSQL.MongoDB
+{
+    public class MongoDbOptions
+    {
+        protected string SectionName { get; }
+
+        public string ConnectionString { get; protected set; }
+
+        public string Host { get; protected set; }
+
+        public string Port { get; protected set; }
+
+        public string Database { get; protected set; }
+
+        public string Collection { get; protected set; }
+
+        public string Username { get; protected set; }
+
+        public string Password { get; protected set; }
+
+        public MongoDbOptions(string host,
+                              string database,
+                              string collection,
+                              string port = "27017",
+                              string username = null,
+                              string password = null)
+        {
+            Host = host ?? throw new ArgumentNullException(nameof(host));
+            Database = database ?? throw new ArgumentNullException(database);
+            Collection = collection ?? throw new ArgumentNullException(collection);
+            Port = port;
+            Username = username;
+            Password = password;
+
+            CreateConnectionString();
+        }
+
+        public MongoDbOptions(IConfiguration configuration, string sectionName = "MongoDb")
+        {
+            SectionName = sectionName;
+
+            new ConfigureFromConfigurationOptions<MongoDbOptions>(
+                configuration.GetSection(SectionName)).Configure(this);
+
+            CreateConnectionString();
+        }
+
+        private void CreateConnectionString()
+        {
+            if (!string.IsNullOrWhiteSpace(Username) &&
+               !string.IsNullOrWhiteSpace(Password))
+                ConnectionString = $"mongodb://{Username}:{Password}@{Host}:{Port}";
+            else
+                ConnectionString = $"mongodb://{Host}:{Port}";
+        }
+    }
+}

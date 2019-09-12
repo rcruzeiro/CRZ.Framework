4e1fe895dd0c4447add3fce24062c5dcbe75de54
Rafael Cruzeiro <rafael.cruzeiro@me.com>
2019-09-12 06:58:17 -0300

    include CRZ.Framework.Utils
    - contains Regex Parser
    - contains Email pattern and validation

---
+++ CRZ.Framework.Utils/RegexParser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CRZ.Framework.Utils
+{
+    public sealed class RegexParser
+    {
+        readonly Regex _pattern;
+
+        public RegexParser(Regex pattern)
+        {
+            _pattern = pattern ?? throw new ArgumentNullException(nameof(pattern));
+        }
+
+        public bool TryParse(string input, out string output)
+        {
+            output = null;
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                var match = _pattern.Match(input);
+
+                if (match != null)
+                {
+                    if (!match.Success) return false;
+
+                    StringBuilder result = new StringBuilder();
+
+                    for (int i = 1; i < match.Groups?.Count; i++)
+                    {
+                        string value = match.Groups[i].Value;
+
+                        if (!string.IsNullOrWhiteSpace(value))
+                            result.Append(value);
+                    }
+
+                    output = result.ToString();
+
+                    return match.Success;
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryParse(string input, out string[] segments)
+        {
+            segments = null;
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                var match = _pattern.Match(input);
+
+                if (match != null && match.Groups != null)
+                {
+                    if (!match.Success) return false;
+
+                    var result = new List<string>();
+
+                    for (int i = 1; i < match.Groups.Count; i++)
+                    {
+                        string value = match.Groups[i].Value;
+
+                        if (!string.IsNullOrWhiteSpace(value))
+                            result.Add(value);
+                    }
+
+                    segments = result.ToArray();
+
+                    return match.Success;
+                }
+            }
+
+            return false;
+        }
+
+        public override string ToString() => $"Pattern: {_pattern.ToString()}";
+    }
+}
---
+++ CRZ.Framework.Utils/ValidationHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CRZ.Framework.Utils
+{
+    public static class ValidationHelper
+    {
+        static readonly Lazy<Regex> _emailPattern =
+            new Lazy<Regex>(() => new Regex(@"^([a-zA-Z0-9!#$%&'*+/=?^{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*)@((?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9])(?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$", RegexOptions.IgnoreCase | RegexOptions.Compiled));
+
+        public static Regex EmailPattern => _emailPattern.Value;
+
+        public static class Common
+        {
+            public static bool IsEmail(string email)
+            {
+                if (string.IsNullOrWhiteSpace(email)) return false;
+
+                return EmailPattern.IsMatch(email);
+            }
+        }
+    }
+}
